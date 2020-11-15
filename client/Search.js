f8010ad823845dc72f520fe6d72053f106674633
Valerie Ingmann <valerieingmann707@gmail.com>
2020-11-14 19:51:26 -0500

    feat: search displays 10 results with poster, title, and year

--- client/Search.js
+++ client/Search.js
@@ -1,31 +1,42 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Search = () => {
-	const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
+  const [results, setResults] = useState([]);
 
-	const handleSubmit = async event => {
-		event.preventDefault();
+  const handleSubmit = async event => {
+    event.preventDefault();
+    // api currently only sends back 10 results. could implement pagination as a future feature
+    let response = await axios.get(`/api/search/${title}`);
+    setResults(response.data.Search);
 
-		let response = await axios.get(`/api/search/${title}`);
-		console.log(response);
+    setTitle("");
+  };
+  console.log(results);
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>Search by Title</label>
+        <input type="text" value={title} onChange={event => setTitle(event.target.value)}></input>
+        <button type="submit" disabled={!title.length}>
+          Submit
+        </button>
+      </form>
 
-		// api currently only sends back 10 results. could implement pagination as a future feature
-
-		setTitle('');
-	};
-
-	return (
-		<>
-			<form onSubmit={handleSubmit}>
-				<label>Search by Title</label>
-				<input type='text' value={title} onChange={event => setTitle(event.target.value)}></input>
-				<button type='submit' disabled={!title.length}>
-					Submit
-				</button>
-			</form>
-		</>
-	);
+      {results.map(result => {
+        let posterElem = result.Poster && result.Poster !== "N/A" && <img src={result.Poster} />;
+        return (
+          <div key={result.imdbID}>
+            <p>
+              {result.Title}, {result.Year}
+            </p>
+            {posterElem}
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Search;
